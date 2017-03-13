4829c31a20d5843096634320e35c3ed09bf75a8d
Danila Leontyev <danila.leontyev.web@gmail.com>
2017-03-13 19:05:41 +0500

    comment

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,23 @@
-// Get the <datalist> and <input> elements.
+
 var dataList = document.getElementById('json-datalist');
 var input = document.getElementById('ajax');
 
-// Create a new XMLHttpRequest.
+
 var request = new XMLHttpRequest();
 
-// Handle state changes for the request.
+
 request.onreadystatechange = function(response) {
     if (request.readyState === 4) {
         if (request.status === 200) {
+
             // Parse the JSON
             var jsonOptions = JSON.parse(request.responseText);
 
             // Loop over the JSON array.
             jsonOptions.forEach(function(item) {
-                // Create a new <option> element.
+
                 var option = document.createElement('option');
-                // Set the value using the item in the JSON array.
+
                 option.value = item.City;
                 // Add the <option> element to the <datalist>.
                 dataList.appendChild(option);
@@ -25,15 +26,15 @@ request.onreadystatechange = function(response) {
             // Update the placeholder text.
             input.placeholder = "Введите или выберите из списка";
         } else {
-            // An error occured :(
+            //  Error
             input.placeholder = "Что-то пошло не так. Проверьте соединение с интернетом и попробуйте еще раз :( :(";
         }
     }
 };
 
 // Update the placeholder text.
-input.placeholder = "Loading options...";
+input.placeholder = "Загрузка ...";
 
-// Set up and make the request.
+
 request.open('GET', '../assets/kladr.json', true);
 request.send();
